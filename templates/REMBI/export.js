aa874859c18295c9d4622d38c3f27fdbff1a79e0
shaunzhao666 <shaun311800@gmail.com>
2023-06-05 13:51:12 -0400

    debug

--- templates/REMBI/export.js
+++ templates/REMBI/export.js
@@ -71,7 +71,7 @@ export default {
 					}
 					exportConfig.outputMatrix.push(outputRow);
 				}
-				let finalMatrix = removeDuplicatesAndCollapse(exportConfig.outputMatrix, exportConfig.uid);
+				let finalMatrix = removeDuplicatesAndCollapse(exportConfig.outputMatrix, "submitter_id");
 				logs.push([`${exportConfig.exportType} information is done`]);
 				exportFile(finalMatrix, exportConfig.exportType, "tsv");
 			}
